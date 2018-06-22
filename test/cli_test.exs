4d414bc0553d10543204cd57500bc4e323532514
Anil Kulkarni <akulkarni@avvo.com>
2018-06-22 15:46:07 -0700

    Fix failing test

--- test/cli_test.exs
+++ test/cli_test.exs
@@ -1,14 +1,20 @@
 defmodule CliTest do
   use ExUnit.Case, async: true
+  alias Wand.CLI
   import Mox
   setup :verify_on_exit!
 
   test "help returns a status code of 1" do
     stub_exit(1)
-    Wand.CLI.main(["help"])
+    stub_display()
+    CLI.main(["help"])
   end
 
   defp stub_exit(status) do
-    expect(Wand.CLI.SystemMock, :halt, fn ^status -> :ok end)
+    expect(CLI.SystemMock, :halt, fn ^status -> :ok end)
+  end
+
+  defp stub_display() do
+    expect(CLI.DisplayMock, :print, fn _message -> :ok end)
   end
 end
