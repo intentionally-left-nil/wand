43117546b782c641325fea2e937c71fdc7896ef5
Anil Kulkarni <akulkarni@avvo.com>
2018-06-22 16:42:41 -0700

    Implement a basic renderer

--- lib/cli/display.ex
+++ lib/cli/display.ex
@@ -6,10 +6,34 @@ defmodule Wand.CLI.Display do
   end
 end
 
+defmodule Wand.CLI.Display.Renderer do
+  alias Earmark.Block.{Heading,Para}
+  alias IO.ANSI
+  def render(blocks, context) do
+    mapper = context.options.mapper
+    text = mapper.(blocks, &(render_block(&1)))
+    |> IO.iodata_to_binary()
+    {context, text}
+  end
+
+  defp render_block(%Heading{content: content}) do
+    [ANSI.underline(), content, ANSI.no_underline()]
+  end
+
+  defp render_block(%Para{lines: lines}) do
+    Enum.join(lines)
+  end
+end
+
 defmodule Wand.CLI.Display.Impl do
   @behaviour Wand.CLI.Display
+
   def print(message) do
-    IO.puts(message)
+    {blocks, context} = Earmark.parse(message)
+    Wand.CLI.Display.Renderer.render(blocks, context)
+    |> elem(1)
+    |> IO.puts
+
     :ok
   end
 end
--- test/display_test.exs
+++ test/display_test.exs
@@ -2,9 +2,14 @@ defmodule DisplayTest do
   use ExUnit.Case, async: true
   import ExUnit.CaptureIO
   alias Wand.CLI.Display.Impl, as: Display
+  alias IO.ANSI
 
   test "print a simple string" do
     assert print("Hello world") == "Hello world\n"
+  end
+
+  test "Adds underline to a title" do
+    assert print("# Hello") == "#{ANSI.underline()}Hello#{ANSI.no_underline()}\n"
   end
 
   defp print(message) do
