8d7e2a1fc0e0c83f6f241d2fc9eda2390a025a22
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-06-22 08:48:55 -0700

    Refactor upgrade test

--- test/arg_parser_test.exs
+++ test/arg_parser_test.exs
@@ -34,46 +34,4 @@ defmodule ArgParserTest do
       assert ArgParser.parse(["version"]) == {:version, []}
     end
   end
-
-  describe "upgrade" do
-    test "returns help if invalid flags are given" do
-      assert ArgParser.parse(["upgrade", "poison", "--wrong-flag"]) ==
-               {:help, :upgrade, {:invalid_flag, "--wrong-flag"}}
-    end
-
-    test "a single package" do
-      assert ArgParser.parse(["upgrade", "poison"]) == {:upgrade, {["poison"], :major}}
-    end
-
-    test "a single package with the shorthand" do
-      assert ArgParser.parse(["u", "poison"]) == {:upgrade, {["poison"], :major}}
-    end
-
-    test "--latest is the same as major" do
-      assert ArgParser.parse(["upgrade", "poison", "--patch", "--latest"]) ==
-               {:upgrade, {["poison"], :major}}
-    end
-
-    test "a single package to the next minor version" do
-      assert ArgParser.parse(["upgrade", "poison", "--minor"]) == {:upgrade, {["poison"], :minor}}
-    end
-
-    test "a single package to the next patch version" do
-      assert ArgParser.parse(["upgrade", "poison", "--patch"]) == {:upgrade, {["poison"], :patch}}
-    end
-
-    test "If both major and minor are passed in, prefer major" do
-      assert ArgParser.parse(["upgrade", "poison", "--minor", "--major"]) ==
-               {:upgrade, {["poison"], :major}}
-    end
-
-    test "upgrade multiple packages" do
-      assert ArgParser.parse(["upgrade", "poison", "ex_doc", "--patch"]) ==
-               {:upgrade, {["poison", "ex_doc"], :patch}}
-    end
-
-    test "upgrade all packages if none passed in" do
-      assert ArgParser.parse(["upgrade", "--patch"]) == {:upgrade, {:all, :patch}}
-    end
-  end
 end
---
+++ test/commands/upgrade_test.exs
@@ -0,0 +1,46 @@
+defmodule UpgradeTest do
+  use ExUnit.Case, async: true
+  alias Wand.CLI.Commands.Upgrade
+
+  describe "validate" do
+    test "returns help if invalid flags are given" do
+      assert Upgrade.validate(["upgrade", "poison", "--wrong-flag"]) ==
+               {:error, {:invalid_flag, "--wrong-flag"}}
+    end
+
+    test "a single package" do
+      assert Upgrade.validate(["upgrade", "poison"]) == {:ok, {["poison"], :major}}
+    end
+
+    test "a single package with the shorthand" do
+      assert Upgrade.validate(["u", "poison"]) == {:ok, {["poison"], :major}}
+    end
+
+    test "--latest is the same as major" do
+      assert Upgrade.validate(["upgrade", "poison", "--patch", "--latest"]) ==
+               {:ok, {["poison"], :major}}
+    end
+
+    test "a single package to the next minor version" do
+      assert Upgrade.validate(["upgrade", "poison", "--minor"]) == {:ok, {["poison"], :minor}}
+    end
+
+    test "a single package to the next patch version" do
+      assert Upgrade.validate(["upgrade", "poison", "--patch"]) == {:ok, {["poison"], :patch}}
+    end
+
+    test "If both major and minor are passed in, prefer major" do
+      assert Upgrade.validate(["upgrade", "poison", "--minor", "--major"]) ==
+               {:ok, {["poison"], :major}}
+    end
+
+    test "upgrade multiple packages" do
+      assert Upgrade.validate(["upgrade", "poison", "ex_doc", "--patch"]) ==
+               {:ok, {["poison", "ex_doc"], :patch}}
+    end
+
+    test "upgrade all packages if none passed in" do
+      assert Upgrade.validate(["upgrade", "--patch"]) == {:ok, {:all, :patch}}
+    end
+  end
+end
