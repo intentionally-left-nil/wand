49d3c863d4f6928249cad22af71b664f20813c1f
Anil Kulkarni <akulkarni@avvo.com>
2018-06-25 15:31:58 -0700

    Validate the version and dependency fields

--- lib/wand_file.ex
+++ lib/wand_file.ex
@@ -1,11 +1,78 @@
 defmodule Wand.WandFile do
   @f Wand.File.impl()
+  @requirement "~> 1.0"
 
-  def load(path \\ "wand.json") do
-    @f.read(path)
-    |> parse
+  defstruct version: "1.0",
+  dependencies: []
+
+  defmodule Dependency do
+    @enforce_keys [:name]
+    defstruct name: nil, requirement: nil, opts: %{}
   end
 
-  defp parse({:ok, contents}), do: Poison.decode(contents)
-  defp parse({:error, _} = error), do: error
+  def load(path \\ "wand.json") do
+    with \
+      {:ok, contents} <- @f.read(path),
+      {:ok, data} <- Poison.decode(contents, keys: :atoms),
+      {:ok, wand_file} <- validate(data)
+    do
+      {:ok, wand_file}
+    else
+      error -> error
+    end
+  end
+
+  defp validate(data) do
+    with \
+      {:ok, version} <- validate_version(extract_version(data)),
+      {:ok, dependencies} <- validate_dependencies(Map.get(data, :dependencies, %{}))
+    do
+      {:ok, %Wand.WandFile{version: to_string(version), dependencies: dependencies}}
+    else
+      error -> error
+    end
+  end
+
+  defp validate_dependencies(dependencies) when not is_map(dependencies), do: {:error, :invalid_dependencies}
+
+  defp validate_dependencies(dependencies) do
+    {dependencies, errors} = Enum.map(dependencies, fn
+      {name, [requirement, opts]} -> create_dependency(name, requirement, opts)
+      {name, requirement} -> create_dependency(name, requirement, %{})
+    end)
+    |> Enum.split_with(fn
+      %Dependency{} -> true
+      _ -> false
+    end)
+
+    case errors do
+      [] -> {:ok, dependencies}
+      [error | _] -> error
+    end
+  end
+
+  defp validate_version({:error, _} = error), do: error
+  defp validate_version({:ok, version}) do
+    if Version.match?(version, @requirement) do
+      {:ok, version}
+    else
+      {:error, :version_mismatch}
+    end
+  end
+
+  defp extract_version(%{version: version}) when is_binary(version) do
+    case Version.parse(version) do
+      :error -> {:error, :invalid_version}
+      {:ok, version} -> {:ok, version}
+    end
+  end
+  defp extract_version(%{version: _}), do: {:error, :invalid_version}
+  defp extract_version(_data), do: {:error, :missing_version}
+
+  defp create_dependency(name, requirement, opts) do
+    case Version.parse_requirement(requirement) do
+      :error -> {:error, {:invalid_dependency, name}}
+      _ -> %Dependency{name: name, requirement: requirement, opts: opts}
+    end
+  end
 end
--- test/wand_file_test.exs
+++ test/wand_file_test.exs
@@ -6,12 +6,12 @@ defmodule WandFileTest do
   describe "load" do
     test "loads the default wand.json file" do
       stub_read_valid()
-      assert WandFile.load() == {:ok, valid_deps()}
+      assert WandFile.load() == {:ok, valid_deps_config()}
     end
 
     test "loads a given path" do
       stub_read_valid("/path/to/wand.json")
-      assert WandFile.load("/path/to/wand.json") == {:ok, valid_deps()}
+      assert WandFile.load("/path/to/wand.json") == {:ok, valid_deps_config()}
     end
 
     test "returns an error if not found" do
@@ -22,6 +22,46 @@ defmodule WandFileTest do
     test "returns an error if not valid JSON" do
       stub_read(:ok, "wand.json", "[ NOT VALID JSON")
       assert WandFile.load() == {:error, {:invalid, "N", 2}}
+    end
+
+    test ":missing_version if the version is missing" do
+      stub_read(:ok, "wand.json", "{\"dependencies\": {}}")
+      assert WandFile.load() == {:error, :missing_version}
+    end
+
+    test ":invalid_version if the version is nil" do
+      json = Poison.encode!(%{version: nil, dependencies: %{}})
+      stub_read(:ok, "wand.json", json)
+      assert WandFile.load() == {:error, :invalid_version}
+    end
+
+    test ":invalid version if the version is bad" do
+      json = Poison.encode!(%{version: "1.0", dependencies: %{}})
+      stub_read(:ok, "wand.json", json)
+      assert WandFile.load() == {:error, :invalid_version}
+    end
+
+    test ":version_mismatch if the version is too high" do
+      json = Poison.encode!(%{version: "2.0.0", dependencies: %{}})
+      stub_read(:ok, "wand.json", json)
+      assert WandFile.load() == {:error, :version_mismatch}
+    end
+
+    test ":invalid_dependencies when the key is not a map" do
+      json = Poison.encode!(%{version: "1.0.0", dependencies: []})
+      stub_read(:ok, "wand.json", json)
+      assert WandFile.load() == {:error, :invalid_dependencies}
+    end
+
+    test ":invalid dependency when a dependency is invalid" do
+      json = Poison.encode!(%{
+        version: "1.0.0",
+        dependencies: %{
+          mox: "== == 1.0.0"
+        }
+      })
+      stub_read(:ok, "wand.json", json)
+      assert WandFile.load() == {:error, {:invalid_dependency, :mox}}
     end
   end
 
@@ -42,11 +82,28 @@ defmodule WandFileTest do
 
   defp valid_deps() do
     %{
-      "version" => "1.0",
-      "dependencies" => %{
-        "mox" => ["~> 0.3.2", %{"only" => "test"}],
-        "poison" => "~> 3.1",
+      version: "1.0.0",
+      dependencies: %{
+        mox: ["~> 0.3.2", %{only: "test"}],
+        poison: "~> 3.1",
       }
+    }
+  end
+
+  defp valid_deps_config() do
+    %WandFile{
+      version: "1.0.0",
+      dependencies: [
+        %WandFile.Dependency{
+          name: :mox,
+          requirement: "~> 0.3.2",
+          opts: %{only: "test"}
+        },
+        %WandFile.Dependency{
+          name: :poison,
+          requirement: "~> 3.1",
+        }
+      ]
     }
   end
 end
