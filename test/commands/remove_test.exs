2b8e7cc8e01319c08b8b1c13600055be2ff380e4
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-06-25 08:00:35 -0700

    Add remove help messages

--- test/commands/remove_test.exs
+++ test/commands/remove_test.exs
@@ -1,5 +1,6 @@
 defmodule RemoveTest do
   use ExUnit.Case, async: true
+  import Mox
   alias Wand.CLI.Commands.Remove
 
   describe "validate" do
@@ -13,6 +14,28 @@ defmodule RemoveTest do
 
     test "returns an array of multiple packages to remove" do
       assert Remove.validate(["remove", "poison", "ex_doc"]) == {:ok, ["poison", "ex_doc"]}
+    end
+  end
+
+  describe "help" do
+    setup :verify_on_exit!
+    setup :stub_io
+
+    test "missing_package" do
+      Remove.help(:missing_package)
+    end
+
+    test "banner" do
+      Remove.help(:banner)
+    end
+
+    test "verbose" do
+      Remove.help(:verbose)
+    end
+
+    def stub_io(_) do
+      expect(Wand.CLI.IOMock, :puts, fn _message -> :ok end)
+      :ok
     end
   end
 end
