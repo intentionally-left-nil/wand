8680a02244c2c02c99dd229c8c42bdcf610c8b68
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-06-28 00:54:45 -0700

    Refactor the tests

--- test/commands/add_execute_test.exs
+++ test/commands/add_execute_test.exs
@@ -119,39 +119,53 @@ defmodule AddExecuteTest do
     end
   end
 
-  test "adds a single package" do
-    Helpers.WandFile.stub_load()
-    Helpers.Hex.stub_poison()
+  describe "Successfully" do
+    setup do
+      Helpers.WandFile.stub_load()
+      :ok
+    end
 
-    %WandFile{
-      dependencies: [%Dependency{name: "poison", requirement: ">= 3.1.0 and < 4.0.0"}]
-    }
-    |> Helpers.WandFile.stub_save()
+    test "adds a single package" do
+      Helpers.Hex.stub_poison()
 
-    assert Add.execute([@poison]) == :ok
-  end
+      %WandFile{
+        dependencies: [%Dependency{name: "poison", requirement: ">= 3.1.0 and < 4.0.0"}]
+      }
+      |> Helpers.WandFile.stub_save()
 
-  test "add a package with a version" do
-    Helpers.WandFile.stub_load()
+      assert Add.execute([@poison]) == :ok
+    end
 
-    %WandFile{
-      dependencies: [%Dependency{name: "poison", requirement: ">= 3.1.3 and < 4.0.0"}]
-    }
-    |> Helpers.WandFile.stub_save()
+    test "add a package with a version" do
+      %WandFile{
+        dependencies: [%Dependency{name: "poison", requirement: ">= 3.1.3 and < 4.0.0"}]
+      }
+      |> Helpers.WandFile.stub_save()
 
-    package = %Package{name: "poison", requirement: ">= 3.1.3 and < 4.0.0"}
-    assert Add.execute([package]) == :ok
-  end
+      package = get_package()
+      assert Add.execute([package]) == :ok
+    end
 
-  test "add a package with the exact version" do
-    Helpers.WandFile.stub_load()
+    test "add a package with the exact version" do
+      %WandFile{
+        dependencies: [%Dependency{name: "poison", requirement: "== 3.1.2"}]
+      }
+      |> Helpers.WandFile.stub_save()
 
-    %WandFile{
-      dependencies: [%Dependency{name: "poison", requirement: "== 3.1.2"}]
-    }
-    |> Helpers.WandFile.stub_save()
+      package = get_package(requirement: "== 3.1.2")
+      assert Add.execute([package]) == :ok
+    end
 
-    package = %Package{name: "poison", requirement: "== 3.1.2"}
-    assert Add.execute([package]) == :ok
+    defp get_package(opts \\ []) do
+      fields = %Package{}
+      |> Map.to_list()
+      |> Keyword.merge([
+        name: "poison",
+        requirement: ">= 3.1.3 and < 4.0.0"
+      ])
+      |> Keyword.merge(opts)
+
+      struct(Package, fields)
+    end
   end
 end
