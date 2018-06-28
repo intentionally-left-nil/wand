3f4d46bcab1724cf3a15a0d51d6c1706d0873fa6
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-06-28 03:36:58 -0700

    Refactor execute to use the new helper

--- lib/cli/commands/add/execute.ex
+++ lib/cli/commands/add/execute.ex
@@ -2,16 +2,21 @@ defmodule Wand.CLI.Commands.Add.Execute do
   alias Wand.CLI.Commands.Add.Package
   alias Wand.WandFile
   alias Wand.WandFile.Dependency
+  alias Wand.CLI.WandFileWithHelp
   alias Wand.CLI.Display
   import Wand.CLI.Errors, only: [error: 1]
 
   def execute(packages) do
-    with {:ok, file} <- load_file(),
+    with {:ok, file} <- WandFileWithHelp.load(),
          {:ok, dependencies} <- get_dependencies(packages),
          {:ok, file} <- add_dependencies(file, dependencies),
-         :ok <- save_file(file) do
+         :ok <- WandFileWithHelp.save(file) do
       :ok
     else
+      {:error, :wand_file_load, reason} ->
+        WandFileWithHelp.handle_error(:wand_file_load, reason)
+      {:error, :wand_file_save, reason} ->
+        WandFileWithHelp.handle_error(:wand_file_save, reason)
       {:error, step, reason} -> handle_error(step, reason)
     end
   end
@@ -100,97 +105,6 @@ defmodule Wand.CLI.Commands.Add.Execute do
     end)
   end
 
-  defp load_file() do
-    case WandFile.load() do
-      {:ok, file} -> {:ok, file}
-      {:error, reason} -> {:error, :wand_file_load, reason}
-    end
-  end
-
-  defp save_file(file) do
-    case WandFile.save(file) do
-      :ok -> :ok
-      {:error, reason} -> {:error, :wand_file_save, reason}
-    end
-  end
-
-  defp handle_error(:wand_file_load, :json_decode_error) do
-    """
-    # Error
-    wand.json is not a valid JSON file.
-    Please make sure you don't have any dangling commas or other invalid json.
-    """
-    |> Display.error()
-
-    error(:invalid_wand_file)
-  end
-
-  defp handle_error(:wand_file_load, reason)
-       when reason in [:invalid_version, :missing_version, :version_mismatch] do
-    """
-    # Error
-    The version field in wand.json is incorrect.
-
-    Please make sure it is present and can be read by this version of the wand cli. You may need to upgrade wand to read this file.
-
-    Detailed error: #{reason}
-    """
-    |> Display.error()
-
-    error(:invalid_wand_file)
-  end
-
-  defp handle_error(:wand_file_load, {:file_read_error, :eaccess}) do
-    """
-    # Error
-    Permission error reading wand.json.
-    Please check to make sure the current user has read permissions, then try again.
-
-    Detailed error: #{:file.format_error(:eaccess)}
-    """
-    |> Display.error()
-
-    error(:missing_wand_file)
-  end
-
-  defp handle_error(:wand_file_load, {:file_read_error, reason}) do
-    """
-    # Error
-    Could not find wand.json in the current directory.
-
-    Make sure you are running `wand add` from the root folder of your project, and that wand.json exists. If you are missing wand.json, type `wand init` to create one.
-
-    Detailed error: #{:file.format_error(reason)}
-    """
-    |> Display.error()
-
-    error(:missing_wand_file)
-  end
-
-  defp handle_error(:wand_file_load, :invalid_dependencies) do
-    """
-    # Error
-    The version field in wand.json is incorrect.
-
-    Either the key is missing, or it is not a map. Please edit the file, and then re-run wand add.
-    """
-    |> Display.error()
-
-    error(:invalid_wand_file)
-  end
-
-  defp handle_error(:wand_file_load, {:invalid_dependency, name}) do
-    """
-    # Error
-    A dependency in wand.json is formatted incorrectly.
-
-    The dependency #{name} in wand.json is incorrect. Please fix it and try again.
-    """
-    |> Display.error()
-
-    error(:invalid_wand_file)
-  end
-
   defp handle_error(:dependency, {:not_found, name}) do
     """
     # Error
@@ -229,19 +143,5 @@ defmodule Wand.CLI.Commands.Add.Execute do
     |> Display.error()
 
     error(:package_already_exists)
-  end
-
-  defp handle_error(:wand_file_save, reason) do
-    """
-    # Error
-    Could not write to wand.json
-
-    Make sure you have permission to write to wand.json, otherwise see the detailed error for more information:
-
-    Detailed error: #{:file.format_error(reason)}
-    """
-    |> Display.error()
-
-    error(:file_write_error)
   end
 end
