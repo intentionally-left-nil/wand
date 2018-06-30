acd1c0aa161a7b36668a36064adbe18ad53d151b
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-06-30 14:57:58 -0700

    make sure to not crash on git dependencies

--- lib/cli/commands/upgrade/execute.ex
+++ lib/cli/commands/upgrade/execute.ex
@@ -54,10 +54,13 @@ defmodule Wand.CLI.Commands.Upgrade.Execute do
   end
 
   defp update_dependency(%Dependency{name: name, requirement: requirement} = dependency, options) do
-    with mode <- Mode.from_requirement(requirement),
+    with {:source, :hex} <- {:source, Dependency.source(dependency)},
+      mode <- Mode.from_requirement(requirement),
          {:ok, requirement} <- update_requirement(dependency, options, mode) do
       {:ok, %Dependency{dependency | requirement: requirement}}
     else
+      # Non-hex dependencies are currently not touched by wand
+      {:source, _source} -> {:ok, dependency}
       {:error, error} -> {:error, {error, name}}
     end
   end
