931faad3b93bad5ff7f23639a26ff38582c1bd68
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-06-30 22:53:39 -0700

    Add error docs

--- lib/cli/error.ex
+++ lib/cli/error.ex
@@ -29,6 +29,29 @@ defmodule Wand.CLI.Error do
     bad_wand_core_version: 75
   }
 
+  @moduledoc """
+  When the CLI has an error, it will return a status code indicative of the type of error that occured.
+  This can be used for scripting. These exit codes are guaranteed to be the same only within the same major version.
+
+  The current error codes are as follows:
+  ```
+  %{
+    missing_wand_file: 64,
+    invalid_wand_file: 65,
+    package_not_found: 66,
+    package_already_exists: 67,
+    hex_api_error: 68,
+    file_write_error: 69,
+    install_deps_error: 70,
+    file_already_exists: 71,
+    wand_core_api_error: 72,
+    mix_file_not_updated: 73,
+    wand_core_missing: 74,
+    bad_wand_core_version: 75
+  }
+  ```
+  """
+
   @spec code(key) :: t
   def code(key), do: Map.fetch!(@errors, key)
 
